66511b0e5bc7b55dc847d3cb6f1f19a5430e9f42
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-09-30 14:25:08 +0500

    init

--- lib/Constants/Colors.dart
+++ lib/Constants/Colors.dart
@@ -9,4 +9,5 @@ mixin MyColors {
   static const primaryDark = Color(0xFF1B2B3D);
   static const primaryExtDark = Color(0xFF192839);
   static const switchColor = Color(0xFFC47171);
+  static const loginBackground = Color(0x4D000000);
 }
