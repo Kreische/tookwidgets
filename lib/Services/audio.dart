55a74bbc8edbe1517996aa84d3e1cbd8cf55758e
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-10-22 18:20:25 +0500

    audio player updated as singleton

--- lib/Services/audio.dart
+++ lib/Services/audio.dart
@@ -1,12 +1,24 @@
 import 'package:just_audio/just_audio.dart';
 
-mixin MyAudioPlayer {
-  static Future play(String string) async {
+class MyAudioPlayer {
+  MyAudioPlayer._singleton() {
+    audioplayer = AudioPlayer();
+  }
+  static MyAudioPlayer instance = MyAudioPlayer._singleton();
+  late AudioPlayer audioplayer;
+
+  Future play(String string) async {
     try {
-      final AudioPlayer audioplayer = AudioPlayer();
       await audioplayer.setAsset(string).then((value) => audioplayer.play());
     } catch (e) {
       return;
     }
+  }
+
+  Future play4Times(String asset) async {
+    await audioplayer.setAsset(asset).then((value) async {
+      await audioplayer.play();
+      print('played');
+    });
   }
 }
