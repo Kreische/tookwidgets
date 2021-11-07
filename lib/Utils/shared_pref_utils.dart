21f3c6aec85ae5f09006b91f9fd0d213e1024234
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-11-07 13:01:58 +0500

    init

--- lib/Utils/shared_pref_utils.dart
+++ lib/Utils/shared_pref_utils.dart
@@ -1,8 +1,24 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tookwidgets/Models/currency.dart';
 
-mixin CommonPrefKey {}
+mixin CommonPrefKey {
+  static const currency = 'currency';
+}
 
 mixin CommonSharedPrefUtils {
+  static Future<bool?> setCurrency(CurrencyData currency) async {
+    final pref = await SharedPreferences.getInstance();
+    final res = pref.setString(CommonPrefKey.currency, currency.toJson());
+    return res;
+  }
+
+  static Future<CurrencyData?> getCurrency() async {
+    final pref = await SharedPreferences.getInstance();
+    final map = pref.getString(CommonPrefKey.currency);
+    if (map == null) return null;
+    return CurrencyData.fromJson(map);
+  }
+
   static Future<bool> clear() async {
     final pref = await SharedPreferences.getInstance();
     final res = await pref.clear();
