1bc8627fd8d6568461a827edab213de659e55540
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-11-10 14:01:23 +0500

    init

--- lib/Constants/PluginStringData.dart
+++ lib/Constants/PluginStringData.dart
@@ -1,4 +1,5 @@
 mixin PluginStringData {
   static const private = 'Private';
   static const taxi = 'Taxi';
+  static const noInternetError = 'No internet connection detected';
 }
---
+++ lib/UI/Widgets/connectivity_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'package:tookwidgets/Constants/Colors.dart';
+import 'package:tookwidgets/Constants/PluginStringData.dart';
+
+import 'TextWidget.dart';
+
+class NoInternetConnectionBar extends StatefulWidget {
+  const NoInternetConnectionBar({Key? key, required this.onConnectionUpdate})
+      : super(key: key);
+  final Function(InternetConnectionStatus?) onConnectionUpdate;
+
+  @override
+  State<NoInternetConnectionBar> createState() =>
+      _NoInternetConnectionBarState();
+}
+
+class _NoInternetConnectionBarState extends State<NoInternetConnectionBar> {
+  late InternetConnectionChecker internetConnectionChecker;
+  @override
+  void initState() {
+    internetConnectionChecker = InternetConnectionChecker();
+    internetConnectionChecker.onStatusChange.listen(widget.onConnectionUpdate);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<InternetConnectionStatus>(
+        stream: internetConnectionChecker.onStatusChange.asBroadcastStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData &&
+              snapshot.data == InternetConnectionStatus.disconnected)
+            return Container(
+              color: MyColors.mainColor,
+              height: 30,
+              alignment: Alignment.center,
+              child: const TextWidget(
+                PluginStringData.noInternetError,
+                color: Colors.white,
+              ),
+            );
+
+          return const SizedBox();
+        });
+  }
+}
