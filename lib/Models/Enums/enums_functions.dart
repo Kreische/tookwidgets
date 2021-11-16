d0bbc9b2ad369f5e6b85f4511264a4fe6ee160b6
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-11-16 19:59:20 +0500

    init

--- lib/Models/Enums/enums_functions.dart
+++
@@ -1,6 +0,0 @@
-String enumToString(Object? o) => o.toString().split('.').last;
-
-T? enumFromString<T>(String? key, List<T> values) {
-  if (key == null) return null;
-  return values.firstWhere((v) => key.split('.').last == enumToString(v));
-}
