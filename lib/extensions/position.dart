baf6e601cc890278657bf26801956e62d3d26488
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-11-22 15:32:42 +0500

    position validator

--- lib/extensions/position.dart
+++ lib/extensions/position.dart
@@ -9,4 +9,19 @@ extension PositionExt on Position? {
         target: getLatlng,
         zoom: 15,
       );
+
+  dynamic validate(data) {
+    return {
+      'latitude': (data['latitude']).toDouble(),
+      'longitude': (data['longitude']).toDouble(),
+      'timestamp': data['timestamp'],
+      'altitude': (data['altitude'] ?? 0.0).toDouble(),
+      'accuracy': (data['accuracy'] ?? 0.0).toDouble(),
+      'heading': (data['heading'] ?? 0.0).toDouble(),
+      'floor': data['floor'],
+      'speed': (data['speed'] ?? 0.0).toDouble(),
+      'speedAccuracy': (data['speed_accuracy'] ?? 0.0).toDouble(),
+      'isMocked': data['is_mocked'] ?? false,
+    };
+  }
 }
