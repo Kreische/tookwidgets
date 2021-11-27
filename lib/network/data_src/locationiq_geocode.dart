c3f4e73ba8c97f7141911fb8d1301d83da4244d6
abeer10 <abeeraamir10@gmail.com>
2021-11-27 17:19:14 +0500

    auto completes

--- lib/network/data_src/locationiq_geocode.dart
+++ lib/network/data_src/locationiq_geocode.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:tookwidgets/models/autocomplete.dart';
 import 'package:tookwidgets/models/location_iq.dart';
 
 class LocationIqGeocode {
@@ -21,11 +22,17 @@ class LocationIqGeocode {
   Future<LocationIq> findAddressesFromCoordinates(
       double lat, double long) async {
     final url = '$_host?key=$apiKey&lat=$lat&lat=$long&format=json';
-    final address = await send(url);
+    final address = await _send(url);
     return LocationIq.fromMap(address);
   }
 
-  Future send(String url) async {
+  Future<List<AutoComplete>> listOfAddresses(String search) async {
+    final url = '$_host?key=$apiKey&q$search';
+    List addresses = await _send(url);
+    return addresses.map((e) => AutoComplete.fromMap(e)).toList();
+  }
+
+  Future _send(String url) async {
     final request = await _dio.get(url);
     final Map data = request.data as Map;
     return data;
