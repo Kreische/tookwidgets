17a714f82f4ae71db43a8d191be395f5219853a2
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-11-28 15:39:33 +0500

    Location Iq complete

--- lib/utils/location_utils.dart
+++ lib/utils/location_utils.dart
@@ -39,12 +39,9 @@ class LocationUtils {
   Future<Address> getAddressFronCoordinates(LatLng coordinates) async {
     try {
       final res = await getIqLocationAddress(coordinates);
-      print('IQ location');
       return res;
     } catch (e) {
-      print(e);
       final googleAddress = await getAddressByGoogle(coordinates);
-      print('GOogle');
       return googleAddress;
     }
   }
