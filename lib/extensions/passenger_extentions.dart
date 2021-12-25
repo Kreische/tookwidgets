31d68390fa3f8d1bc83b1321d1ff30e1a2bd3e0d
Muhamad Khuram <64301140+muhamadkhuram@users.noreply.github.com>
2021-12-25 19:55:32 +0500

    passenger social update

--- lib/extensions/passenger_extentions.dart
+++ lib/extensions/passenger_extentions.dart
@@ -13,4 +13,17 @@ extension PassengerSocialExt on PassengerSocial {
         return PluginStringData.iLikeQuiteRide;
     }
   }
+
+  PassengerSocial get next {
+    switch (this) {
+      case PassengerSocial.iLikeToChat:
+        return PassengerSocial.eitherWay;
+      case PassengerSocial.eitherWay:
+        return PassengerSocial.iLikeQuiteRide;
+      case PassengerSocial.iLikeQuiteRide:
+        return PassengerSocial.iLikeToChat;
+      default:
+        return PassengerSocial.eitherWay;
+    }
+  }
 }
