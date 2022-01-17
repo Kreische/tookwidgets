1c8101609ec32e9d576248300c5c2ecb00e76784
naved-shk <naveedshaikh1599@gmail.com>
2022-01-17 11:50:58 +0530

    init

--- lib/models/currency.dart
+++ lib/models/currency.dart
@@ -1,50 +1,101 @@
 import 'dart:convert';
 
+// class CurrencyData {
+//   CurrencyData({
+//     required this.country,
+//     required this.countryCode,
+//     required this.currency,
+//     required this.currencyCode,
+//   });
+
+//   factory CurrencyData.fromMap(map) {
+//     return CurrencyData(
+//       country: map['country'] as String,
+//       countryCode: map['countryCode'] as String,
+//       currency: map['currency'] as String,
+//       currencyCode: map['currencyCode'] as String,
+//     );
+//   }
+
+//   factory CurrencyData.fromJson(String source) =>
+//       CurrencyData.fromMap(json.decode(source));
+
+//   final String country;
+//   final String countryCode;
+//   final String currency;
+//   final String currencyCode;
+
+//   CurrencyData copyWith({
+//     String? country,
+//     String? countryCode,
+//     String? currency,
+//     String? currencyCode,
+//   }) {
+//     return CurrencyData(
+//       country: country ?? this.country,
+//       countryCode: countryCode ?? this.countryCode,
+//       currency: currency ?? this.currency,
+//       currencyCode: currencyCode ?? this.currencyCode,
+//     );
+//   }
+
+//   Map<String, dynamic> get toMap => {
+//         'country': country,
+//         'countryCode': countryCode,
+//         'currency': currency,
+//         'currencyCode': currencyCode,
+//       };
+
+//   String toJson() => json.encode(toMap);
+// }
+
 class CurrencyData {
   CurrencyData({
-    required this.country,
-    required this.countryCode,
-    required this.currency,
-    required this.currencyCode,
+    required this.code,
+    required this.name,
+    required this.symbol,
+    required this.flag,
+    required this.number,
+    required this.decimalDigits,
+    required this.namePlural,
+    required this.symbolOnLeft,
+    required this.decimalSeparator,
+    required this.thousandsSeparator,
+    required this.spaceBetweenAmountAndSymbol,
   });
 
-  factory CurrencyData.fromMap(map) {
-    return CurrencyData(
-      country: map['country'] as String,
-      countryCode: map['countryCode'] as String,
-      currency: map['currency'] as String,
-      currencyCode: map['currencyCode'] as String,
-    );
-  }
+  ///The currency code
+  final String code;
 
-  factory CurrencyData.fromJson(String source) =>
-      CurrencyData.fromMap(json.decode(source));
+  ///The currency name in English
+  final String name;
 
-  final String country;
-  final String countryCode;
-  final String currency;
-  final String currencyCode;
+  ///The currency symbol
+  final String symbol;
 
-  CurrencyData copyWith({
-    String? country,
-    String? countryCode,
-    String? currency,
-    String? currencyCode,
-  }) {
-    return CurrencyData(
-      country: country ?? this.country,
-      countryCode: countryCode ?? this.countryCode,
-      currency: currency ?? this.currency,
-      currencyCode: currencyCode ?? this.currencyCode,
-    );
-  }
+  ///The currency flag code
+  ///
+  /// To get flag unicode(Emoji) use [CurrencyUtils.currencyToEmoji]
+  final String flag;
 
-  Map<String, dynamic> get toMap => {
-        'country': country,
-        'countryCode': countryCode,
-        'currency': currency,
-        'currencyCode': currencyCode,
-      };
+  ///The currency number
+  final int number;
 
-  String toJson() => json.encode(toMap);
+  ///The currency decimal digits
+  final int decimalDigits;
+
+  ///The currency plural name in English
+  final String namePlural;
+
+  ///The decimal separator
+  final String decimalSeparator;
+
+  ///The thousands separator
+  final String thousandsSeparator;
+
+  ///True if symbol is on the Left of the amount
+  final bool symbolOnLeft;
+
+  ///True if symbol has space with amount
+  final bool spaceBetweenAmountAndSymbol;
 }
