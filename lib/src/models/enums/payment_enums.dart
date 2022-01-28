7ae8ed87db5e874e2fbdb4031b512ae284948da6
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-01-28 17:48:18 +0500

    Removed InApp Payment method

--- lib/src/models/enums/payment_enums.dart
+++ lib/src/models/enums/payment_enums.dart
@@ -1,15 +1,21 @@
 import 'package:tookwidgets/src/plugin_consts/plugin_asset_strings.dart';
 import 'package:tookwidgets/took_widgets.dart';
 
-enum PaymentMethod { inApp, cash, cardMachine, bankTransfer }
+enum PaymentMethod {
+  //  inApp,
+
+  cash,
+  cardMachine,
+  bankTransfer
+}
 
 extension PaymentExt on PaymentMethod {
   String get toMap => enumToString(this);
 
   Tuple<String, String> get config {
     switch (this) {
-      case PaymentMethod.inApp:
-        return Tuple('assets/Icons/in_app_pay.png', 'In App');
+      // case PaymentMethod.inApp:
+      //   return Tuple('assets/Icons/in_app_pay.png', 'In App');
       case PaymentMethod.cash:
         return Tuple(PluginAssetString.cashPayment, 'Cash');
       case PaymentMethod.bankTransfer:
@@ -23,8 +29,8 @@ extension PaymentExt on PaymentMethod {
 
   String get askAboutAcceptance {
     switch (this) {
-      case PaymentMethod.inApp:
-        return 'Do you accept in app payment';
+      // case PaymentMethod.inApp:
+      //   return 'Do you accept in app payment';
       case PaymentMethod.cash:
         return PluginStringData.acceptCashpayments;
       case PaymentMethod.bankTransfer:
@@ -38,8 +44,8 @@ extension PaymentExt on PaymentMethod {
 
   String get asset {
     switch (this) {
-      case PaymentMethod.inApp:
-        return 'assets/Icons/in_app_pay.png';
+      // case PaymentMethod.inApp:
+      //   return 'assets/Icons/in_app_pay.png';
       case PaymentMethod.cash:
         return PluginAssetString.cashPayment;
       case PaymentMethod.bankTransfer:
@@ -53,8 +59,8 @@ extension PaymentExt on PaymentMethod {
 
   String get amountToShow {
     switch (this) {
-      case PaymentMethod.inApp:
-        return PluginStringData.inAppPayAmount;
+      // case PaymentMethod.inApp:
+      //   return PluginStringData.inAppPayAmount;
       case PaymentMethod.cash:
         return PluginStringData.cashAmount;
       case PaymentMethod.bankTransfer:
