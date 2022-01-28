b3080e4afb1458207144a30bb1e1ec8ad8640e86
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-01-28 11:12:57 +0500

    enum function

--- lib/src/models/enums/enums_functions.dart
+++ lib/src/models/enums/enums_functions.dart
@@ -2,6 +2,6 @@ String enumToString(Object? o) => o.toString().split('.').last;
 
 T? enumFromString<T>(String? key, List<T> values) {
   if (key == null) return null;
-  if (values.every((e) => e.toString().split('.').last != key)) return null;
-  return values.firstWhere((v) => key.split('.').last == enumToString(v));
+  if (values.every((e) => enumToString(e) != enumToString(key))) return null;
+  return values.firstWhere((v) => enumToString(key) == enumToString(v));
 }
