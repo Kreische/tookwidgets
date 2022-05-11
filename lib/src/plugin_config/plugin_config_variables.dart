bc5b49e44b0cefc1ad37215d3d2206da72a8e8c7
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-05-11 17:35:48 +0500

    removed google geocode

--- lib/src/plugin_config/plugin_config_variables.dart
+++
@@ -1,4 +0,0 @@
-mixin PluginConfigVariable {
-  static const googleApiKey = 'AIzaSyDoMByrBVZmqV3mppbFixGsBaX7MVQsKnU';
-  static const iqLocationKey = 'pk.8d697e0f6bd9a848a04c641039c77e59';
-}
--- lib/src/utils/updater_utils.dart
+++ lib/src/utils/updater_utils.dart
@@ -1,63 +1,61 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:new_version/new_version.dart';
-import 'package:tookwidgets/src/models/address.dart';
+// import 'package:flutter/material.dart';
+// import 'package:new_version/new_version.dart';
+// import 'package:tookwidgets/src/models/address.dart';
 
-import 'location_utils.dart';
+// enum AppName { driver, passenger }
 
-enum AppName { driver, passenger }
+// extension AppNameExt on AppName {
+//   String get id {
+//     switch (this) {
+//       case AppName.driver:
+//         const driverIOS = 'com.betesla.driver';
+//         const driverAndroid = 'com.took.driver';
+//         return Platform.isAndroid ? driverAndroid : driverIOS;
+//       case AppName.passenger:
+//         const passengerIOS = 'com.betesla.passenger';
+//         const passengerAndroid = 'com.betesla.passenger';
+//         return Platform.isAndroid ? passengerAndroid : passengerIOS;
+//       default:
+//         return '';
+//     }
+//   }
+// }
 
-extension AppNameExt on AppName {
-  String get id {
-    switch (this) {
-      case AppName.driver:
-        const driverIOS = 'com.betesla.driver';
-        const driverAndroid = 'com.took.driver';
-        return Platform.isAndroid ? driverAndroid : driverIOS;
-      case AppName.passenger:
-        const passengerIOS = 'com.betesla.passenger';
-        const passengerAndroid = 'com.betesla.passenger';
-        return Platform.isAndroid ? passengerAndroid : passengerIOS;
-      default:
-        return '';
-    }
-  }
-}
+// mixin UpdaterUtils {
+//   static Future checkForUpdate(BuildContext context, AppName name) async {
+//     Address? address;
+//     if (Platform.isIOS) {
+//       address = await LocationUtils.instance.run();
+//     }
+//     final version = NewVersion(
+//       androidId: name.id,
+//       iOSId: name.id,
+//       iOSAppStoreCountry: address?.countryCode,
+//     );
 
-mixin UpdaterUtils {
-  static Future checkForUpdate(BuildContext context, AppName name) async {
-    Address? address;
-    if (Platform.isIOS) {
-      address = await LocationUtils.instance.run();
-    }
-    final version = NewVersion(
-      androidId: name.id,
-      iOSId: name.id,
-      iOSAppStoreCountry: address?.countryCode,
-    );
+//     final versionStatus = await version.getVersionStatus();
+//     if (versionStatus == null) return;
+//     final uptodate = versionStatus.localVersion == versionStatus.storeVersion;
+//     if (uptodate) return;
+//     version.showUpdateDialog(context: context, versionStatus: versionStatus);
+//   }
 
-    final versionStatus = await version.getVersionStatus();
-    if (versionStatus == null) return;
-    final uptodate = versionStatus.localVersion == versionStatus.storeVersion;
-    if (uptodate) return;
-    version.showUpdateDialog(context: context, versionStatus: versionStatus);
-  }
+//   static Future<bool> isUpdatedAppVersion(AppName name) async {
+//     Address? address;
+//     if (Platform.isIOS) {
+//       address = await LocationUtils.instance.run();
+//     }
+//     final version = NewVersion(
+//       androidId: name.id,
+//       iOSId: name.id,
+//       iOSAppStoreCountry: address?.countryCode,
+//     );
 
-  static Future<bool> isUpdatedAppVersion(AppName name) async {
-    Address? address;
-    if (Platform.isIOS) {
-      address = await LocationUtils.instance.run();
-    }
-    final version = NewVersion(
-      androidId: name.id,
-      iOSId: name.id,
-      iOSAppStoreCountry: address?.countryCode,
-    );
+//     final versionStatus = await version.getVersionStatus();
 
-    final versionStatus = await version.getVersionStatus();
-
-    final uptodate = versionStatus?.localVersion == versionStatus?.storeVersion;
-    return uptodate;
-  }
-}
+//     final uptodate = versionStatus?.localVersion == versionStatus?.storeVersion;
+//     return uptodate;
+//   }
+// }
