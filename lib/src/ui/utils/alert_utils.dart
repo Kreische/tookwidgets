78205245be84b01a35fcf4631aaad29456cdb3ec
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-05-18 21:53:50 +0500

    alert dialog native look

--- lib/src/ui/utils/alert_utils.dart
+++ lib/src/ui/utils/alert_utils.dart
@@ -1,26 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tookwidgets/src/ui/widget/buttons.dart';
 import 'package:tookwidgets/src/ui/widget/text_widget.dart';
+import 'package:tookwidgets/took_widgets.dart';
 
 mixin AlertUtils {
   static void alert(
     BuildContext context, {
     required String title,
-    required String? content,
+    required String content,
+    String actionBtnText = 'Ok',
   }) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
+    if (Platform.isAndroid) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          title: TextWidget(title),
+          content: TextWidget(content),
+          actions: [
+            TookBTN(onPressed: () => Navigator.pop(context)),
+          ],
         ),
-        title: TextWidget(title),
-        content: TextWidget(content),
-        actions: [
-          TookBTN(onPressed: () => Navigator.pop(context)),
-        ],
-      ),
-    );
+      );
+    }
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+          title: TextWidget(title),
+          content: TextWidget(content),
+          actions: [
+            CupertinoDialogAction(
+              onPressed: () => Navigator.pop(context),
+              child: TextWidget(
+                actionBtnText,
+                color: MyColors.accentColor,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   static void snakBar(BuildContext context, {required String content}) {
