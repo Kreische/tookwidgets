a172e95125aac0ebf56d76633ff5bf4242c96279
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-06-07 11:04:56 +0500

    position get camera position remove 0,0 latlng, set sydney location

--- lib/src/extensions/position.dart
+++ lib/src/extensions/position.dart
@@ -2,7 +2,10 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 extension PositionExt on Position? {
-  LatLng get getLatlng => LatLng(this?.latitude ?? 0, this?.longitude ?? 0);
+  LatLng get getLatlng => LatLng(
+        this?.latitude ?? -33.836252,
+        this?.longitude ?? 151.236971,
+      );
 
   CameraPosition get getCameraPosition => CameraPosition(
         target: getLatlng,
