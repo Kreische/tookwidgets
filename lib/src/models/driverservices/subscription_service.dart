ed387464e3c95dfaa4f58f4f008477704d2e9fbc
muhamadkhuram <muhamadkhuram207@gmail.com>
2022-07-16 14:33:42 +0500

    init

--- lib/src/models/driverservices/subscription_service.dart
+++ lib/src/models/driverservices/subscription_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
+
 import 'package:tookwidgets/src/models/amount.dart';
 import 'package:tookwidgets/src/models/working_days.dart';
 
@@ -11,7 +12,7 @@ class Subscription {
     this.enabled = false,
     this.availableQuantity = 0,
     this.price = const Amount(),
-    this.workingDays = const WorkingDays(),
+    this.availability = const SubscriptionAvailability(),
   });
 
   factory Subscription.fromMap(map) {
@@ -20,7 +21,7 @@ class Subscription {
       enabled: (map['enabled'] ?? false) as bool,
       availableQuantity: (map['availableQuantity']?.toInt() ?? 0) as int,
       price: Amount.fromMap(map['price']),
-      workingDays: WorkingDays.fromMap(map['workingDays']),
+      availability: SubscriptionAvailability.fromMap(map['availability']),
     );
   }
 
@@ -30,19 +31,19 @@ class Subscription {
   final bool enabled;
   final int availableQuantity;
   final Amount price;
-  final WorkingDays workingDays;
+  final SubscriptionAvailability availability;
 
   Subscription copyWith({
     bool? enabled,
     int? availableQuantity,
     Amount? price,
-    WorkingDays? workingDays,
+    SubscriptionAvailability? availability,
   }) {
     return Subscription(
       enabled: enabled ?? this.enabled,
       availableQuantity: availableQuantity ?? this.availableQuantity,
       price: price ?? this.price,
-      workingDays: workingDays ?? this.workingDays,
+      availability: availability ?? this.availability,
     );
   }
 
@@ -50,7 +51,7 @@ class Subscription {
         'enabled': enabled,
         'availableQuantity': availableQuantity,
         'price': price.toMap,
-        'workingDays': workingDays.toMap,
+        'availability': availability.toMap,
       };
 
   String toJson() => json.encode(toMap);
@@ -63,7 +64,7 @@ class Subscription {
         other.enabled == enabled &&
         other.availableQuantity == availableQuantity &&
         other.price == price &&
-        other.workingDays == workingDays;
+        other.availability == availability;
   }
 
   @override
@@ -72,7 +73,76 @@ class Subscription {
       enabled,
       availableQuantity,
       price,
+      availability,
+    ]);
+  }
+}
+
+@immutable
+class SubscriptionAvailability {
+  const SubscriptionAvailability({
+    this.workingDays = const WorkingDays(),
+    this.startTime,
+    this.endTime,
+  });
+
+  factory SubscriptionAvailability.fromMap(map) {
+    return SubscriptionAvailability(
+      workingDays: WorkingDays.fromMap(map['workingDays']),
+      startTime: map['startTime'] == null
+          ? null
+          : DateTime.parse(map['startTime'] as String),
+      endTime: map['endTime'] == null
+          ? null
+          : DateTime.parse(map['endTime'] as String),
+    );
+  }
+
+  factory SubscriptionAvailability.fromJson(String source) =>
+      SubscriptionAvailability.fromMap(json.decode(source));
+
+  final WorkingDays workingDays;
+  final DateTime? startTime;
+  final DateTime? endTime;
+
+  SubscriptionAvailability copyWith({
+    WorkingDays? workingDays,
+    DateTime? startTime,
+    DateTime? endTime,
+  }) {
+    return SubscriptionAvailability(
+      workingDays: workingDays ?? this.workingDays,
+      startTime: startTime ?? this.startTime,
+      endTime: endTime ?? this.endTime,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'workingDays': workingDays.toMap,
+      'startTime': startTime?.toUtc(),
+      'endTime': endTime?.toUtc(),
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is SubscriptionAvailability &&
+        other.workingDays == workingDays &&
+        other.startTime == startTime &&
+        other.endTime == endTime;
+  }
+
+  @override
+  int get hashCode {
+    return hashList([
       workingDays,
+      startTime,
+      endTime,
     ]);
   }
 }
